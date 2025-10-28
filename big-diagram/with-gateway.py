f26b41f9f723dd18d99cc9a489711f9b061cfd86
agustinafassina <agustinafassina@gmail.com>
2025-10-27 23:07:49 -0300

    new big diagram

---
+++ big-diagram/with-gateway.py
@@ -0,0 +1,58 @@
+import json
+from diagrams import Diagram, Cluster, Node
+from diagrams.aws.compute import EC2, ECS, Lambda, AutoScaling
+from diagrams.aws.network import ELB, Route53, APIGateway
+from diagrams.aws.database import RDS
+from diagrams.aws.security import IAMRole, WAF
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.storage import S3
+
+with open('config.json') as f:
+    config = json.load(f)
+
+region_name = config['region']
+components = config['components']
+roles = components['roles']
+security_groups = components['security_groups']
+file_name = "with-gateway-diagram"
+
+with Diagram(file_name, show=True):
+    with Cluster(f"Region: {region_name}"):
+        vpc = Cluster("VPC")
+        with vpc:
+            route53 = Route53(components['route53'])
+            gateway = APIGateway(components['api_gateway'])
+            lambda_name = Lambda(components['lambda_name'])
+            load_balancer = ELB(components['load_balancer'])
+            ecs_service = ECS(components['ecs_service'])
+            ec2_instance = EC2(components['ec2_instance'])
+            database = RDS(components['database'])
+            cloudwatch = Cloudwatch(components['cloudwatch'])
+            ec2_role = IAMRole(roles['ec2_role'])
+            ecs_role = IAMRole(roles['ecs_role'])
+            rds_sg = Node(security_groups['rds_sg'])
+            ec2_sg = Node(security_groups['ec2_sg'])
+            lambda_logs = Cloudwatch("Lambda Logs")
+            gateway_logs = Cloudwatch("API Gateway Logs")
+            auto_scaling = AutoScaling("Auto Scaling Group")
+            waf = WAF(components.get('waf', 'WAF'))
+            s3_bucket_ec2 = S3(components.get('s3_bucket_ec2', 'S3 Bucket'))
+            s3_bucket_backup = S3(components.get('s3_bucket_backup', 'S3 Bucket'))
+
+            route53 >> gateway
+            gateway >> gateway_logs
+            gateway >> lambda_name
+            lambda_name >> lambda_logs
+            gateway >> load_balancer
+            load_balancer >> waf
+            load_balancer >> ecs_service
+            ecs_service >> ec2_instance
+            ec2_instance >> auto_scaling
+            ec2_instance >> database
+            ec2_instance >> ec2_role
+            ecs_service >> ecs_role
+            database >> rds_sg
+            ec2_instance >> ec2_sg
+            ec2_instance >> s3_bucket_ec2
+            database >> cloudwatch
+            database >> s3_bucket_backup
